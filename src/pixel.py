9f2c16e9f97dea6cabf86edd0cd6fa153031e09e
jwCheng28 <chengjw68@gmail.com>
2021-03-11 13:38:50 -0800

    Init version of blocks

--- src/pixel.py
+++
@@ -1,9 +0,0 @@
-class Pixel:
-    def __init__(self, loc, color):
-        self.loc = loc
-        self.color = color
-        self.speed_x = 0
-        self.speed_y = -1
-
-    def get_loc(self):
-        return self.loc
