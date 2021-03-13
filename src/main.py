420ae6a5fe934d114728b0b1633ddc267837debf
jwCheng28 <chengjw68@gmail.com>
2021-03-13 14:33:26 -0800

    Updated main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 from pygame.constants import USEREVENT
 import board
 import block
+import game
 
 # Color Constant
 WHITE = (255, 255, 255)
@@ -18,16 +19,7 @@ clock = pygame.time.Clock()
 FPS = 12
 
 # Initialize Board
-gameBoard = board.Board(15, 10, 40, WIN, 140, 100)
-s_block = block.S_shape()
-z_block = block.Z_shape()
-l_block = block.L_shape()
-lm_block = block.L_shape_mirror()
-t_block = block.T_shape()
-square_block = block.Square_shape()
-line_block = block.Line_shape()
-gameBoard.place_block(t_block, 7, 4)
-gameBoard.place_block(s_block, 3, 2)
+myGame = game.Game(surface=WIN)
 
 # Events
 MOVE_DOWN = USEREVENT + 1
@@ -35,33 +27,21 @@ pygame.time.set_timer(MOVE_DOWN, 400)
 
 
 run = True
+myGame.drop_random_blocks()
 while run:
     clock.tick(FPS)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            pygame.display.quit()
-            pygame.quit()
         elif event.type == MOVE_DOWN:
-            gameBoard.move_curr_block("DOWN")
+            myGame.gameBoard.move_curr_block("DOWN")
 
-    keys_pressed = pygame.key.get_pressed()
-    if keys_pressed[pygame.K_UP]:
-        gameBoard.rotate_curr_block()
-    elif keys_pressed[pygame.K_DOWN]:
-        gameBoard.move_curr_block("DOWN")
-    elif keys_pressed[pygame.K_LEFT]:
-        gameBoard.move_curr_block("LEFT")
-    elif keys_pressed[pygame.K_RIGHT]:
-        gameBoard.move_curr_block("RIGHT")
-    elif keys_pressed[pygame.K_ESCAPE]:
-        run = False
-        pygame.display.quit()
-        pygame.quit()
+    run = myGame.block_movement()
 
     WIN.fill(WHITE)
-    gameBoard.render_all()
+    myGame.render_game()
     pygame.display.update()
 
 
+pygame.display.quit()
+pygame.quit()
