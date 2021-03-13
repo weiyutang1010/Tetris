e37d97d91018bccd3dff7450e130b7128f559c96
jwCheng28 <chengjw68@gmail.com>
2021-03-13 14:30:14 -0800

    Added class game to manage all elements

---
+++ src/game.py
@@ -0,0 +1,47 @@
+import pygame
+import board
+import block
+import random
+
+class Game():
+    def __init__(self,
+                 surface,
+                 board_size=(15, 10),
+                 block_size=40,
+                 board_start=(140,100)
+                 ):
+
+        self.gameBoard = board.Board(*board_size, block_size, surface, *board_start)
+        self.board_mid = (0, board_size[1]//2)
+        self.blocks = [
+            block.S_shape(),
+            block.Z_shape(),
+            block.L_shape(),
+            block.L_shape_mirror(),
+            block.T_shape(),
+            block.Square_shape(),
+            block.Line_shape()
+        ]
+
+    def drop_random_blocks(self):
+        count = len(self.blocks)
+        choice = random.randint(0, count-1)
+        self.gameBoard.place_block(self.blocks[choice], *self.board_mid)
+
+    def block_movement(self):
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_UP]:
+            self.gameBoard.rotate_curr_block()
+        elif keys_pressed[pygame.K_DOWN]:
+            self.gameBoard.move_curr_block("DOWN")
+        elif keys_pressed[pygame.K_LEFT]:
+            self.gameBoard.move_curr_block("LEFT")
+        elif keys_pressed[pygame.K_RIGHT]:
+            self.gameBoard.move_curr_block("RIGHT")
+        elif keys_pressed[pygame.K_ESCAPE]:
+            return False
+        return True
+
+    def render_game(self):
+        self.gameBoard.render_all()
+
