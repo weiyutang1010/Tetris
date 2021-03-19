f9acfe9a14f0f0715abd813b9929b94d78297cce
weiyutang <weiyutang1010@outlook.com>
2021-03-19 17:53:07 +0800

    Working on delay

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ pygame.display.set_caption("Tetris!")
 
 # Pygame FPS
 clock = pygame.time.Clock()
-FPS = 8
+FPS = 12
 
 # Initialize Board
 myGame = game.Game(surface=WIN)
@@ -35,7 +35,6 @@ while run:
             myGame.quit()
         elif event.type == MOVE_DOWN:
             myGame.gameBoard.move_curr_block("DOWN")
-
     
     if (myGame.gameBoard.at_bottom()):
         myGame.drop_random_blocks()
