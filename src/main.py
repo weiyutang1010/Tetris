52fdfc660b6e43bef73f18d20925e5ba2d5352fa
weiyutang <weiyutang1010@outlook.com>
2021-03-20 04:45:24 +0800

    Added time for block to move left/right at bottom

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ while run:
         elif event.type == MOVE_DOWN:
             myGame.gameBoard.move_curr_block("DOWN")
     
-    if (myGame.gameBoard.at_bottom()):
+    if (myGame.block_at_bottom()):
         myGame.drop_random_blocks()
     else:
         run = myGame.block_movement()
